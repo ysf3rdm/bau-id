e54788963b9d606c5c60454032599438595772bd
brick-ninja <davidliang0424@gmail.com>
2022-07-04 17:48:40 +0800

    fix the issue for regular expression

--- src/components/SearchName/Search.js
+++ src/components/SearchName/Search.js
@@ -62,15 +62,14 @@ function Search({
           const errors = {}
           if (values.searchKey.length < 3) {
             errors.searchKey = 'Name length must be at least 3 characters'
-          } else if (
-            // !new RegExp(nospecial).test(
-            //   values.searchKey
-            // )
-            !nospecial.test(values.searchKey)
-          ) {
+          } else if (!nospecial.test(values.searchKey)) {
             errors.searchKey =
               'Name can only contain lowercase letters, numbers and emojis'
-          } else if (values.searchKey.indexOf(' ') >= 0) {
+          } else if (
+            values.searchKey.indexOf(' ') >= 0 ||
+            values.searchKey.indexOf('/') >= 0 ||
+            values.searchKey.indexOf('.') >= 0
+          ) {
             errors.searchKey =
               'Name can only contain lowercase letters, numbers and emojis'
           }
