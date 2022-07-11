c1e42248473f6a51c41254a66169d686284432f1
brick-ninja <davidliang0424@gmail.com>
2022-07-11 10:52:03 +0800

    installing ga

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -2,8 +2,8 @@ import ReactGA from 'react-ga'
 import { getNetworkId } from '@siddomains/ui'
 
 const TrackingID = {
-  live: 'UA-138903307-1',
-  dev: 'UA-138903307-2'
+  live: 'G-ZZZT2KP807',
+  dev: 'G-ZZZT2KP807'
 }
 
 function isProduction() {
