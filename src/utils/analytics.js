c335105436ea9289f445cf01d9d1a848cf424859
kevin-222 <110803206+kevin-222@users.noreply.github.com>
2022-10-17 17:09:00 +0800

    feat: ga4 (#224)
    Co-authored-by: kevin-222 <kevin-222@space.id>

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,9 +1,9 @@
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga4'
 import { getNetworkId } from 'ui'
 
 const TrackingID = {
-  live: 'UA-233964103-3',
-  dev: 'UA-233964103-1',
+  live: 'G-T851Y5QX1V',
+  dev: 'G-Y8W54ERLDR',
 }
 
 function isProduction() {
@@ -41,13 +41,17 @@ export function getUtm() {
 export const setupAnalytics = () => {
   if (isProduction()) {
     ReactGA.initialize(TrackingID.live)
-    ReactGA.plugin.require('ecommerce')
+    // ReactGA.plugin.require('ecommerce')
   } else {
     ReactGA.initialize(TrackingID.dev)
-    ReactGA.plugin.require('ecommerce', { debug: true })
+    // ReactGA.plugin.require('ecommerce', { debug: true })
     console.log('Analytics setup for dev with ', TrackingID.dev)
   }
-  ReactGA.pageview(window.location.pathname + window.location.search)
+  // ReactGA.pageview(window.location.pathname + window.location.search)
+  ReactGA.send({
+    hitType: 'pageview',
+    page: window.location.pathname + window.location.search,
+  })
   setUtm()
 }
 
